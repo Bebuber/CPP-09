ce7c19e83555828a2c1b57a7c09a630c2dea32fd
bebuber <bebuber@student.42heilbronn.de>
2025-03-14 15:30:18 +0100

    building the structure

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bebuber <bebuber@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/14 14:46:23 by bebuber           #+#    #+#             */
+/*   Updated: 2025/03/14 15:19:40 by bebuber          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <string>
+#include <map>
+
+class BitcoinEchange {
+	public:
+
+	private:
+	
+}
+
+#endif
